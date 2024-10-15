32ebd5125eb61dfe92f961d4da369d5c1452c4aa
Marisel <shenapam3@gmail.com>
2024-10-15 15:35:40 +0300

    I

--- set.js
+++ set.js
@@ -9,12 +9,12 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Marisel",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "254740007567",              
+    OWNER_NAME: process.env.OWNER_NAME || "Sir Antony",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255779333974",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
-    BOT : process.env.BOT_NAME || '𝐁𝐔𝐆𝐀𝐓𝐓𝐈',
-    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/0418ae5a1e6440e54652c.jpg,https://telegra.ph/file/c425b0fd9ec4ab130c8f9.jpg.https://telegra.ph/file/0d3b89f01e8fccb260b45.jpg.https://telegra.ph/file/abc304e66c2a3e8b2a557.jpg',
+    BOT : process.env.BOT_NAME || 'Immortal_dragon',
+    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/abd0tu.jpg,https://files.catbox.moe/b9qtzn.jpg',
     MODE: process.env.PUBLIC_MODE || "no",
                   
     PM_PERMIT: process.env.PM_PERMIT || 'no',
